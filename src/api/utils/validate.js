dbc33d7e5ce5cbcdf5e4867d25b005abfade3636
jomar <jomar.magno026@gmail.com>
2021-05-02 18:53:18 +0800

    Supports validation for specific element only

--- src/api/utils/validate.js
+++ src/api/utils/validate.js
@@ -20,6 +20,17 @@ const checkInput = (fieldName, requirements, value) => {
     errors.push(`${fieldName} must be string`);
   }
 
+  if (requirements?.array && !isEmpty(value)) {
+    const arrayReq = requirements?.array ?? [];
+    if (!arrayReq.includes(value)) {
+      errors.push(
+        `${fieldName} must be ${arrayReq
+          .filter((entry) => !isEmpty(entry))
+          .join(', ')} only`
+      );
+    }
+  }
+
   if (errors.length > 0) {
     return { [fieldName]: errors };
   }
@@ -30,36 +41,42 @@ const checkInput = (fieldName, requirements, value) => {
  *
  * Generate validation errors base on the give validation data
  */
-const validate = (data) => (req, res, next) => {
-  let errorEntries = {};
+const validate = (data) => {
+  return (req, res, next) => {
+    try {
+      let errorEntries = {};
 
-  const dataEntries = Object.keys(data);
+      const dataEntries = Object.keys(data);
 
-  dataEntries.forEach((dataEntry) => {
-    const entries = Object.keys(data[dataEntry]);
-    const requestValues = Object.keys(req[dataEntry]);
+      dataEntries.forEach((dataEntry) => {
+        const entries = Object.keys(data[dataEntry]);
+        const requestValues = Object.keys(req[dataEntry]);
 
-    entries.forEach((entry) => {
-      const errors = checkInput(
-        entry,
-        data[dataEntry][entry],
-        req[dataEntry][entry]
-      );
+        entries.forEach((entry) => {
+          const errors = checkInput(
+            entry,
+            data[dataEntry][entry],
+            req[dataEntry][entry]
+          );
 
-      if (!isEmpty(errors)) {
-        errorEntries = { ...errorEntries, ...errors };
+          if (!isEmpty(errors)) {
+            errorEntries = { ...errorEntries, ...errors };
+          }
+        });
+      });
+
+      if (!isEmpty(errorEntries)) {
+        throw new APIError({
+          status: httpStatus.BAD_REQUEST,
+          message: 'Validation Error',
+          errors: errorEntries,
+        });
       }
-    });
-  });
-
-  if (!isEmpty(errorEntries)) {
-    throw new APIError({
-      status: httpStatus.BAD_REQUEST,
-      message: 'Validation Error',
-      errors: errorEntries,
-    });
-  }
-  next();
+      next();
+    } catch (error) {
+      next(error);
+    }
+  };
 };
 
 module.exports = validate;
